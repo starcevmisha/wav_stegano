2ab30bf50cbed4e11e78d221d629fab53631aadf
Starcev <starcev_misha@mail.ru>
2017-10-31 20:40:19 +0500

    Работает с одним наименшим битом

--- wav_steg.py
+++ wav_steg.py
@@ -1,81 +1,107 @@
 import wave
 import struct
-from bitstring import BitArray
 
-def hide():
+def hide(input_file, file_to_hide, output_file, lsb):
     num_lsb = 1
 
-    sound = wave.open("song_short.wav")
+    sound = wave.open(input_file)
     params = sound.getparams()
-    num_frames = sound.getnframes()
-    num_channels = params.nchannels
-    num_samples = num_frames * num_channels
-    a = sound.readframes(params.nframes)
+    frames = sound.readframes(params.nframes)
     sound.close()
 
     samples_num = params.nframes * params.nchannels
     if params.sampwidth == 1:
         sample_format = "{}B".format(samples_num)
-        mask = (1 << 15) - (1 << num_lsb)
+        mask = (1 << 8) - (1 << num_lsb)
     elif params.sampwidth == 2:
-        mask = (1 << 15) - (1 << num_lsb)
+        mask = (1 << 16) - (1 << num_lsb)
         sample_format = "{}H".format(samples_num)
     elif params.sampwidth == 3:
-        sample_format = "{}B".format(samples_num*3)
+        mask = (1 << 24) - (1 << num_lsb)
+        sample_format = "{}B".format(samples_num * 3)
 
 
 
     max_bytes_hide = (samples_num * num_lsb) // 8
-    print(max_bytes_hide)
+    print("You can hide only {} bytes".format(max_bytes_hide - 4))
 
-    sound_data = struct.unpack(sample_format, a)
+    sound_data = struct.unpack(sample_format, frames)
+
+
+
+
+    with open(file_to_hide, "rb") as f:
+        input_data = f.read()
+    print("Your files: {} bytes".format(len(input_data)))
+    input_data = struct.pack('I',len(input_data)) + input_data
+    bit_input_data = bits(input_data)
 
     res_data = []
-
-    with open("pal1.bmp", "rb") as f:
-        input_data = BitArray(f.read())
-    print(len(input_data))
     for i in range(len(sound_data)):
-        if i < len(input_data):
-            res_data.append(struct.pack('H',(sound_data[i]&mask)|input_data[i]))
+        if i < len(input_data)*8:
+            res_data.append(struct.pack(sample_format[-1], (sound_data[i] & mask) | next(bit_input_data)))
         else:
-            res_data.append(struct.pack('H', sound_data[i]))
+            res_data.append(struct.pack(sample_format[-1], sound_data[i]))
 
-    print("Ok")
-    sound_steg = wave.open("123.wav", "w")
+    sound_steg = wave.open(output_file, "w")
     sound_steg.setparams(params)
     sound_steg.writeframes(b"".join(res_data))
     sound_steg.close()
+    print("hide done!")
 
-##############################33
-def recover():
-    in_sound = wave.open("123.wav", "r")
+
+def recover(input_file, num_lsb):
+    in_sound = wave.open(input_file, "r")
     params = in_sound.getparams()
-    num_lsb = 1
     samples_num = params.nframes * params.nchannels
+
     if params.sampwidth == 1:
         sample_format = "{}B".format(samples_num)
     elif params.sampwidth == 2:
         sample_format = "{}H".format(samples_num)
     elif params.sampwidth == 3:
-        sample_format = "{}B".format(samples_num*3)
+        sample_format = "{}B".format(samples_num * 3)
     mask = (1 << num_lsb) - 1
 
-    res_data = b''
     raw_data = list(struct.unpack(sample_format, in_sound.readframes(params.nframes)))
-    buffer = 0
-    for i in range(len(raw_data)):
-        if i<9000:
-            if i %8 == 0 and i!=0:
-                res_data+=struct.pack('1B', buffer)
-                buffer = 0
-            buffer = buffer<<1|raw_data[i]&mask
-        else:
-            break
-    with open('output.bmp', 'wb') as f:
+
+    data_len = calculate_length(mask, raw_data)
+    res_data = extract_data(data_len, mask, raw_data)
+    with open('output.txt', 'wb') as f:
         f.write(res_data)
+    print("recover done!")
+
+
+def extract_data(data_len, mask, raw_data):
+    buffer = 0
+    res_data = b''
+    for i in range(33, data_len * 8 + 33):
+        if i % 8 == 0 and i != 0:
+            res_data += struct.pack('1B', buffer)
+            buffer = 0
+        buffer = buffer << 1 | raw_data[i] & mask
+    return res_data
+
+def calculate_length(mask, raw_data):
+    buffer = 0
+    data_len_bytes = []
+    for i in range(32):
+        if i % 8 == 0 and i != 0:
+            data_len_bytes.append(buffer)
+            buffer = 0
+        buffer = buffer << 1 | raw_data[i] & mask
+    data_len_bytes.append(buffer)
+    data_len = struct.unpack('<I', bytes(data_len_bytes))[0]
+    print(data_len)
+    return data_len
+
+def bits(bytes_data):
+    for b in bytes_data:
+        for i in range(7,-1,-1):
+            yield (b >> i) & 1
 
 
 if __name__ == "__main__":
-    hide()
-    recover()
+    hide("hs.wav", "pal1.bmp", "output.wav", 1)
+    recover("output.wav", 1)
+
